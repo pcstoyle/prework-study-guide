d4ffaafcb444ed0eda46026eefc12c87cd215aae
pcstoyle <pcstoyle@hotmail.com>
2023-08-18 10:28:46 -0600

    playing with javascript

---
+++ assets/script.js
@@ -0,0 +1,23 @@
+// var topics = "HTML, CSS, Git, JavaScript, Misc";
+// console.log(topics);
+
+// var topic = "HTML";
+
+// if (topic === 'HTML') {
+//   console.log("Let's study HTML!");
+// } else if (topic === 'CSS') {
+//   console.log("Let's study CSS!");
+// } else if (topic === 'Git') {
+//   console.log("Let's study Git!");
+// } else if (topic === 'JavaScript') {
+//   console.log("Let's study JavaScript!");
+// } else {
+//   console.log('Please try again!');
+// };
+
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+for (let i = 0; i < topics.length; i++) {
+    // const element = array[i];
+    console.log(topics[i]);
+}
